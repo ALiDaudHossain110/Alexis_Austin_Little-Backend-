ed3462f8de91252e0e4260dd2cba2f861357a114
Ali Daud Hossain <137883956+ALiDaudHossain110@users.noreply.github.com>
2025-11-29 01:54:37 +0600

    stupid marketing spending2

--- app/Models/Leadsource.php
+++ app/Models/Leadsource.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\LawFirm;
 
 class LeadSource  extends Model
 {
