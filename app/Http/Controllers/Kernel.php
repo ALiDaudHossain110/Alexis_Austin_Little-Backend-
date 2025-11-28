654a4906301b500c50e597f4e5d7b01d9e6ebc59
Ali Daud Hossain <137883956+ALiDaudHossain110@users.noreply.github.com>
2025-11-28 18:15:20 +0600

    Update Kernel.php

--- app/Http/Controllers/Kernel.php
+++ app/Http/Controllers/Kernel.php
@@ -6,10 +6,10 @@ use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
 class Kernel extends HttpKernel
 {
+
     protected $middleware = [
-        // global middleware
         \App\Http\Middleware\TrustProxies::class,
-        \Fruitcake\Cors\HandleCors::class,
+        \Illuminate\Http\Middleware\HandleCors::class,
         \App\Http\Middleware\PreventRequestsDuringMaintenance::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
